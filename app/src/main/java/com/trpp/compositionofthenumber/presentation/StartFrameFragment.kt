c06aa0d6b9db91b5051b14a5fedf64e04a955e6c
Elizaveta Urvanova <urvanova03@mail.ru>
2023-04-21 21:13:09 +0300

    feat: added navigation in code

--- app/src/main/java/com/trpp/compositionofthenumber/presentation/StartFrameFragment.kt
+++ app/src/main/java/com/trpp/compositionofthenumber/presentation/StartFrameFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.compositionofthenumber.R
 import com.example.compositionofthenumber.databinding.FragmentStartFrameBinding
 
 class StartFrameFragment: Fragment() {
@@ -18,6 +21,13 @@ class StartFrameFragment: Fragment() {
     ): View {
         binding = FragmentStartFrameBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.startGameButton.setOnClickListener{
+            findNavController().navigate(R.id.action_startFrameFragment_to_levelChooseFragment)
+        }
     }
 
 }
