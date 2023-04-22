6b5a99d6e31ac7e60ee065328b3710432430290e
Elizaveta Urvanova <urvanova03@mail.ru>
2023-04-22 21:47:22 +0300

    feat: added timer

--- app/src/main/java/com/trpp/compositionofthenumber/presentation/GameProcessFragment.kt
+++ app/src/main/java/com/trpp/compositionofthenumber/presentation/GameProcessFragment.kt
@@ -5,11 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.compositionofthenumber.databinding.FragmentGameProcessBinding
+import com.trpp.compositionofthenumber.domain.entity.Level
 
 
 class GameProcessFragment : Fragment() {
     private lateinit var binding: FragmentGameProcessBinding
+    private val viewModelFactory by lazy {
+        GameViewModelFactory(Level.EASY, requireActivity().application)
+    }
+
+    private val viewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[GameProcessViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +27,13 @@ class GameProcessFragment : Fragment() {
     ): View {
         binding = FragmentGameProcessBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.formattedTime.observe(viewLifecycleOwner){
+            binding.timer.text = it.toString()
+        }
     }
 }
---
+++ app/src/main/java/com/trpp/compositionofthenumber/presentation/GameProcessViewModel.kt
@@ -0,0 +1,66 @@
+package com.trpp.compositionofthenumber.presentation
+
+import android.app.Application
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.trpp.compositionofthenumber.data.GameRepositoryImpl
+import com.trpp.compositionofthenumber.domain.entity.GameSettings
+import com.trpp.compositionofthenumber.domain.entity.Level
+import com.trpp.compositionofthenumber.domain.usecases.GenerateQuestionUseCase
+import com.trpp.compositionofthenumber.domain.usecases.GetGameSettingsUseCase
+
+
+class GameProcessViewModel(
+    private val context: Application,
+    private val level: Level
+) : ViewModel() {
+    private lateinit var gameSettings: GameSettings
+    private val repository = GameRepositoryImpl
+
+    private val generateQuestionUseCase = GenerateQuestionUseCase(repository)
+    private val getGameSettingsUseCase = GetGameSettingsUseCase(repository)
+    private val _formattedTime = MutableLiveData<String>()
+    val formattedTime: LiveData<String>
+        get() = _formattedTime
+    private var timer: CountDownTimer? = null
+
+    companion object {
+
+        private const val MILLIS_IN_SECONDS = 1000
+        private const val SECONDS_IN_MINUTES = 60
+
+    }
+    init {
+        getGameSettings()
+        startTimer()
+    }
+
+    private fun formatTime(millisUntilFinished: Long): String {
+        val seconds = millisUntilFinished / MILLIS_IN_SECONDS
+        val minutes = seconds / SECONDS_IN_MINUTES
+        val leftSeconds = seconds - (minutes * SECONDS_IN_MINUTES)
+        return String.format("%02d:%02d", minutes, leftSeconds)
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(
+            (gameSettings.gameTimeInSeconds * MILLIS_IN_SECONDS).toLong(),
+            MILLIS_IN_SECONDS.toLong()
+        ) {
+            override fun onTick(millisUntilFinished: Long) {
+                _formattedTime.value = formatTime(millisUntilFinished)
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+        timer?.start()
+    }
+    private fun getGameSettings() {
+        this.gameSettings = getGameSettingsUseCase(level)
+        //_minPercent.value = gameSettings.minPercentOfRightAnswers
+    }
+}
