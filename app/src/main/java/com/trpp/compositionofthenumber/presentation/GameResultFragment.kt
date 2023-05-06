b98c6e6ba4de3883f3dabac6f9bff53136149c4a
Vasyan <vasyan.coder@gmail.com>
2023-05-07 00:15:57 +0300

    feat: set up GameResultFragment

--- app/src/main/java/com/trpp/compositionofthenumber/presentation/GameResultFragment.kt
+++ app/src/main/java/com/trpp/compositionofthenumber/presentation/GameResultFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.compositionofthenumber.R
 import com.example.compositionofthenumber.databinding.FragmentGameResultBinding
 
 
 class GameResultFragment : Fragment() {
     private lateinit var binding: FragmentGameResultBinding
+
+    private val args by navArgs<GameResultFragmentArgs>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +23,34 @@ class GameResultFragment : Fragment() {
     ): View {
         binding = FragmentGameResultBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.restartButton.setOnClickListener {
+            findNavController().navigate(
+                GameResultFragmentDirections.actionGameResultFragmentToDifficultyChooseFragment()
+            )
+        }
+        binding.chooseLevelButton.setOnClickListener {
+            findNavController().navigate(
+                GameResultFragmentDirections.actionGameResultFragmentToLevelChooseFragment()
+            )
+        }
+
+        binding.textCurrentScore.text =
+            getString(R.string.current_score, args.gameResult.countOfRightAnswers.toString())
+
+        if (args.gameResult.winner) {
+            binding.imageWinner.setImageResource(R.drawable.win_sticker)
+        } else {
+            binding.imageWinner.setImageResource(R.drawable.win_sova)
+        }
+
+        val progressPercent: Int =
+            args.gameResult.countOfRightAnswers * 100 / args.gameResult.countOfQuestions
+
+        binding.progressBar.progress = progressPercent
     }
 }
