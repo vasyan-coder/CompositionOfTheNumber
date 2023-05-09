b7331f1a937616fe301a751e1712cddf1b8fb652
Vasyan <vasyan.coder@gmail.com>
2023-05-09 14:10:41 +0300

    feat: added args gameSettings in GameProcessFragment

--- app/src/main/java/com/trpp/compositionofthenumber/presentation/fragments/DifficultyChooseFragment.kt
+++ app/src/main/java/com/trpp/compositionofthenumber/presentation/fragments/DifficultyChooseFragment.kt
@@ -8,10 +8,14 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.compositionofthenumber.R
 import com.example.compositionofthenumber.databinding.FragmentDifficultyChooseBinding
+import com.trpp.compositionofthenumber.data.GameRepositoryImpl
+import com.trpp.compositionofthenumber.domain.entity.Level
 
 class DifficultyChooseFragment : Fragment() {
 
     private lateinit var binding: FragmentDifficultyChooseBinding
+
+    private val repository = GameRepositoryImpl
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +29,25 @@ class DifficultyChooseFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.easyGameTypeButton.setOnClickListener {
-            findNavController().navigate(R.id.action_difficultyChooseFragment_to_gameProcessFragment)
+            findNavController().navigate(
+                DifficultyChooseFragmentDirections.actionDifficultyChooseFragmentToGameProcessFragment(
+                    repository.getGameSettings(Level.EASY)
+                )
+            )
+        }
+        binding.mediumGameTypeButton.setOnClickListener {
+            findNavController().navigate(
+                DifficultyChooseFragmentDirections.actionDifficultyChooseFragmentToGameProcessFragment(
+                    repository.getGameSettings(Level.NORMAL)
+                )
+            )
+        }
+        binding.hardGameTypeButton.setOnClickListener {
+            findNavController().navigate(
+                DifficultyChooseFragmentDirections.actionDifficultyChooseFragmentToGameProcessFragment(
+                    repository.getGameSettings(Level.HARD)
+                )
+            )
         }
     }
 }
